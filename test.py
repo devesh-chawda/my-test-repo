073deca5989df731c3606fc29d242df1ae56c92d
Devesh Chawda <mr.deveshchawda@gmail.com>
2026-09-17 14:42:57 +0530

    test

---
+++ test.py
@@ -0,0 +1,11 @@
+x='this is a string for getting raipur position and letter'
+
+y=x.find('raipur')
+print(y)
+print(len('raipur'))
+print(x[y:(y+len('raipur'))])
+i=x.split(' ')
+print(i)
+i[-1]='words'
+z= ' '.join(i)
+print(z)
